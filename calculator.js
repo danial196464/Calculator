b5bc0d667ffa08e12ac57d57f020d879c7b25e5d
Danial <107314818+danial196464@users.noreply.github.com>
2022-12-19 20:08:10 +0200

    Files Uploaded

---
+++ calculator.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req,res){
+  res.sendFile(__dirname + "/index.html");
+})
+app.post("/",function(req,res){
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+  var result = num1+num2;
+  res.send("Calculated number is: "+result);
+})
+
+app.listen(3000);
+
+app.get("/bmicalculator",function(req,res){
+  res.sendFile(__dirname+"/bmiCalculator.html")
+})
+app.post("/bmicalculator",function(req,res){
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height);
+  var bmi = weight/(height*height);
+  res.send("Your BMI is "+bmi);
+})
